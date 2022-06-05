aef384a1430ef56220200902d20e86841213220b
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2022-06-06 00:11:44 +0300

    Работают две формулы из 4

--- Converter.py
+++ Converter.py
@@ -1,1 +1,44 @@
-print('Hello world')
+def convert_wave_number_to_frequency_when_receiving(wave:int, transponder:int):
+    start_receive_freq = 3_400_000
+    stop_receive_freq = 3_900_000
+    offset_wave = wave - (5000*(transponder-1)+1500)
+    # print(f'{offset_wave=} first')
+    if offset_wave > 0:
+        offset_freq = wave * 10 - 15000
+    else:
+        offset_wave = wave + (5000*(transponder-1)+1500)
+        offset_freq = offset_wave * 10 - 15000
+    freq = start_receive_freq + offset_freq
+    if start_receive_freq < freq < stop_receive_freq:
+        return freq
+    else:
+        return 'Проверьте номер волны, он задан неверно'
+
+
+
+def convert_wave_number_to_frequency_when_transmitting(wave:int, transponder:int):
+    duplex_spacing = 2_325_000
+    starting_transmitting_freq = 3_400_000 + duplex_spacing
+    stopping_transmitting_freq = 3_900_000 + duplex_spacing
+    offset_wave = wave - (5000*(transponder-1)+1500)
+    # print(f'{offset_wave=} first')
+    if offset_wave > 0:
+        offset_freq = wave * 10 - 15000
+    else:
+        offset_wave = wave + (5000*(transponder-1)+1500)
+        offset_freq = offset_wave * 10 - 15000
+    freq = starting_transmitting_freq + offset_freq
+    if starting_transmitting_freq < freq < stopping_transmitting_freq:
+        return freq
+    else:
+        return 'Проверьте номер волны, он задан неверно'
+
+
+if __name__ == "__main__":
+    a = convert_wave_number_to_frequency_when_receiving(770, 4)
+    b = convert_wave_number_to_frequency_when_receiving(17270, 4)
+    print(a == b)
+    c = convert_wave_number_to_frequency_when_transmitting(770, 4)
+    d = convert_wave_number_to_frequency_when_transmitting(17270, 4)
+    print(d)
+    print(c == d)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
 from user_interface import Ui_MainWindow
+from Converter import convert_wave_number_to_frequency_when_receiving as cwn_rc
+from Converter import convert_wave_number_to_frequency_when_transmitting as cwn_ts
 
 
 class Currency_conv(QtWidgets.QMainWindow):
@@ -15,9 +17,38 @@ class Currency_conv(QtWidgets.QMainWindow):
     def init_UI(self):
         self.setWindowTitle("Конвертер волн в частоты и обратно")
         self.setWindowIcon(QIcon("pictures/icons8-ок-16.png"))
+        self.ui.input_vawe_or_frequency.setPlaceholderText('Например: 17270')
+        self.ui.output_vawe_or_frequency.setPlaceholderText('Результат:')
+        self.ui.button_converter.clicked.connect(self.convert_value)
 
-app = QtWidgets.QApplication([])
-application = Currency_conv()
-application.show()
+    def convert_value(self):
+        input_value = self.checking_the_correctness_of_the_entered_data()
+        input_formula = self.checking_combo_box()
+        input_transponder = int(self.ui.choose_transponder.currentText()[-1])
+        answer = input_formula(input_value, input_transponder)
+        self.ui.output_vawe_or_frequency.setText(str(answer))
 
-sys.exit(app.exec())
+    def checking_the_correctness_of_the_entered_data(self):
+        try:
+            input_value = int(self.ui.input_vawe_or_frequency.text())
+            return input_value
+        except ValueError:
+            self.ui.output_vawe_or_frequency.setText('Вы должны ввести целое число')     
+
+    def checking_combo_box(self):
+        dictionary_with_formulas = {"Номер волны в КГц (ПРМ)": cwn_rc,
+                                    "Номер волны в КГц (ПРД)": cwn_ts,
+                                    "КГц в номер волны (ПРМ)": '',
+                                    "КГц в номер волны (ПРД)": ''}
+        input_formula = self.ui.choose_action.currentText()
+        return dictionary_with_formulas.get(input_formula)
+
+def main():
+    app = QtWidgets.QApplication([])
+    application = Currency_conv()
+    application.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
