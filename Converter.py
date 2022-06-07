87fbed0ed59426a4de2617f28ada5e28cd26b251
aleksey88bsuir <aleksey88bsuir@yandex.ru>
2022-06-07 23:35:08 +0300

    Дописал еще 2 формулы, исправил баги

--- Converter.py
+++ Converter.py
@@ -9,7 +9,7 @@ def convert_wave_number_to_frequency_when_receiving(wave:int, transponder:int):
         offset_wave = wave + (5000*(transponder-1)+1500)
         offset_freq = offset_wave * 10 - 15000
     freq = start_receive_freq + offset_freq
-    if start_receive_freq < freq < stop_receive_freq:
+    if start_receive_freq <= freq <= stop_receive_freq:
         return freq
     else:
         return 'Проверьте номер волны, он задан неверно'
@@ -28,17 +28,47 @@ def convert_wave_number_to_frequency_when_transmitting(wave:int, transponder:int
         offset_wave = wave + (5000*(transponder-1)+1500)
         offset_freq = offset_wave * 10 - 15000
     freq = starting_transmitting_freq + offset_freq
-    if starting_transmitting_freq < freq < stopping_transmitting_freq:
+    if starting_transmitting_freq <= freq <= stopping_transmitting_freq:
         return freq
     else:
         return 'Проверьте номер волны, он задан неверно'
 
+    
+def convert_frequency_in_wave_number_when_transmitting(frequency:int, transponder:int):
+    duplex_spacing = 2_325_000
+    starting_transmitting_freq = 3_400_000 + duplex_spacing
+    stopping_transmitting_freq = 3_900_000 + duplex_spacing
+    if not (starting_transmitting_freq <= frequency <= stopping_transmitting_freq):
+        return 'Проверьте частоту, он задана неверно'
+    offset_freq = frequency - starting_transmitting_freq
+    offset_wave = (offset_freq + 15000) / 10
+    return int(offset_wave)
+
+def convert_frequency_in_wave_number_when_receiving(frequency:int, transponder:int):
+    start_receive_freq = 3_400_000
+    stop_receive_freq = 3_900_000
+    if not (start_receive_freq <= frequency <= stop_receive_freq):
+        return 'Проверьте частоту, он задана неверно'
+    offset_freq = frequency - start_receive_freq
+    offset_wave = (offset_freq + 15000) / 10
+    # offset_wave = wave - (5000*(transponder-1)+1500)
+    rx_wave = offset_wave - (5000*(transponder-1)+1500)
+    if int(rx_wave) > 5000 or int(rx_wave) < 0:
+        return 'Проверьте частоту или номер ствола'
+    return int(rx_wave)
+
 
 if __name__ == "__main__":
     a = convert_wave_number_to_frequency_when_receiving(770, 4)
-    b = convert_wave_number_to_frequency_when_receiving(17270, 4)
-    print(a == b)
-    c = convert_wave_number_to_frequency_when_transmitting(770, 4)
+    # b = convert_wave_number_to_frequency_when_receiving(17270, 4)
+    # print(a == b)
+    # c = convert_wave_number_to_frequency_when_transmitting(770, 4)
     d = convert_wave_number_to_frequency_when_transmitting(17270, 4)
-    print(d)
-    print(c == d)
+    # print(c)
+    # print(d)
+    # # print(d)
+    # print(c == d)
+    # e = convert_frequency_in_wave_number_when_transmitting(5725000, 4)
+    # i = convert_frequency_in_wave_number_when_receiving(3900000, 10)
+    print (convert_frequency_in_wave_number_when_receiving(a, 4))
+    print (convert_frequency_in_wave_number_when_transmitting(d, 4))
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt5.QtGui import QIcon
 from user_interface import Ui_MainWindow
 from Converter import convert_wave_number_to_frequency_when_receiving as cwn_rc
 from Converter import convert_wave_number_to_frequency_when_transmitting as cwn_ts
+from Converter import convert_frequency_in_wave_number_when_transmitting as cfw_tx
+from Converter import convert_frequency_in_wave_number_when_receiving as cfw_rx
 
 
 class Currency_conv(QtWidgets.QMainWindow):
@@ -22,24 +24,35 @@ class Currency_conv(QtWidgets.QMainWindow):
         self.ui.button_converter.clicked.connect(self.convert_value)
 
     def convert_value(self):
-        input_value = self.checking_the_correctness_of_the_entered_data()
-        input_formula = self.checking_combo_box()
-        input_transponder = int(self.ui.choose_transponder.currentText()[-1])
-        answer = input_formula(input_value, input_transponder)
-        self.ui.output_vawe_or_frequency.setText(str(answer))
+        check_value, input_value = self.\
+            checking_the_correctness_of_the_entered_data()
+        if check_value:
+            input_formula = self.checking_combo_box()
+            input_transponder = int(self.ui.choose_transponder.\
+                currentText()[-1])
+            if input_transponder == 0:
+                input_transponder = 10 
+            answer = input_formula(input_value, input_transponder)
+            self.ui.output_vawe_or_frequency.setText(str(answer))
 
     def checking_the_correctness_of_the_entered_data(self):
         try:
             input_value = int(self.ui.input_vawe_or_frequency.text())
-            return input_value
+            if input_value < 0:
+                self.ui.output_vawe_or_frequency.setText('Волна не может' +\
+                     ' быть отрицательной')
+                return False, input_value
+            return True, input_value
         except ValueError:
-            self.ui.output_vawe_or_frequency.setText('Вы должны ввести целое число')     
+            self.ui.output_vawe_or_frequency.setText('Вы должны ввести' +\
+                     ' целое число')
+            return False, 0     
 
     def checking_combo_box(self):
         dictionary_with_formulas = {"Номер волны в КГц (ПРМ)": cwn_rc,
                                     "Номер волны в КГц (ПРД)": cwn_ts,
-                                    "КГц в номер волны (ПРМ)": '',
-                                    "КГц в номер волны (ПРД)": ''}
+                                    "КГц в номер волны (ПРМ)": cfw_rx,
+                                    "КГц в номер волны (ПРД)": cfw_tx}
         input_formula = self.ui.choose_action.currentText()
         return dictionary_with_formulas.get(input_formula)
 
